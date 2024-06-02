b551ca483e62e1a5b8c8262f72d4c3072b943c43
Amy <amy_hung@systex.com>
2024-06-02 14:00:19 +0800

    feat: 文章內頁
    1. 新增文章內頁
    2. 使用 useUser 判斷使用者是否為文章作者，以顯示編輯和刪除按鈕。
    3.. 新增 getArticle api，根據文章 ID 獲取文章內容，並做相應的錯誤處理

--- pages/article/[id]/index.vue
+++ pages/article/[id]/index.vue
@@ -1,9 +1,125 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+  const route = useRoute();
+  const articleId = route.params.id;
+  const { data: article, error } = await useFetch(`/api/article/getArticle?id=${articleId}`);
+  if (error.value) {
+    throw createError({
+      statusCode: error.value.statusCode,
+      message: error.value.statusMessage,
+    });
+  }
+
+  useSeoMeta({
+    title: article.value ? `${sliceContent(article.value.title, 30)}` : "載入中…",
+    ogTitle: article.value ? `${sliceContent(article.value.title, 30)} - 文章詳情` : "載入中…",
+    description: article.value ? sliceContent(article.value.content, 100) : "載入中…",
+    ogDescription: article.value ? sliceContent(article.value.content, 100) : "載入中…",
+    ogImage: article.value?.cover ?? "/starlight-background.jpg",
+    twitterCard: "summary_large_image",
+  });
+
+  const isArticleAuthor = ref(false);
+  const useUser = useUserStore();
+
+  if (article.value?.authorId === useUser.userInfo.id) {
+    isArticleAuthor.value = true;
+  } else {
+    isArticleAuthor.value = false;
+  }
+
+  const deleteArticle = async () => {};
+</script>
 
 <template>
-  <div>
-    Page: article\[id]\index
+  <div class="container mt-3">
+    <div class="row justify-content-center">
+      <div class="col-lg-11 col-12">
+        <div class="d-flex justify-content-between align-items-center mb-4">
+          <NuxtLink to="/article/list" class="btn btn-secondary">
+            <i class="bi bi-arrow-left"></i> 返回列表頁
+          </NuxtLink>
+          <div v-if="isArticleAuthor">
+            <NuxtLink :to="`/article/${route.params.id}/edit`" class="btn btn-edit me-2">
+              <i class="bi bi-pencil-square"></i> 編輯
+            </NuxtLink>
+            <button class="btn btn-outline-danger me-2" @click="deleteArticle">
+              <i class="bi bi-trash"></i> 刪除
+            </button>
+          </div>
+        </div>
+        <article>
+          <header class="mb-4">
+            <h1 class="article-title mb-3 display-4 fw-bold">{{ article?.title }}</h1>
+            <div class="d-flex align-items-center mb-3">
+              <div class="text-muted fst-italic me-3">
+                <i class="bi bi-calendar-event me-1"></i>
+                {{ article?.date }}
+              </div>
+              <NuxtLink :to="`/user/${article?.authorId}`" class="text-muted fst-italic">
+                <i class="bi bi-person-fill me-1"></i>
+                {{ article?.authorName }}
+              </NuxtLink>
+            </div>
+            <div class="mb-3">
+              <NuxtLink
+                v-for="tag in article?.tags"
+                :key="tag"
+                class="badge bg-secondary text-decoration-none link-light me-2 mb-2"
+                :to="'/topic/' + tag">
+                <i class="bi bi-tag-fill me-1"></i>
+                {{ tag }}
+              </NuxtLink>
+            </div>
+          </header>
+          <figure v-if="article?.cover != null" class="article-cover mb-4 text-center">
+            <NuxtImg
+              :src="article?.cover"
+              class="img-fluid rounded mx-auto w-100 h-100"
+              :alt="article?.title"
+              quality="90"
+              format="webp"
+              loading="lazy" />
+          </figure>
+          <section class="mb-5">
+            <p class="fs-5 mb-4 text-break">
+              {{ article?.content }}
+            </p>
+          </section>
+        </article>
+      </div>
+    </div>
   </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+  .btn-edit {
+    border-color: darkmagenta;
+    color: darkmagenta;
+  }
+  .btn-edit:hover {
+    background-color: darkmagenta;
+    color: whitesmoke;
+  }
+
+  .article-title {
+    color: darkblue;
+    word-break: break-word;
+    overflow-wrap: break-word;
+    max-width: 100%;
+  }
+
+  .article-cover {
+    width: 100%;
+    height: 600px;
+  }
+
+  .comments-list .comment-item {
+    border-bottom: 1px solid #dee2e6;
+    padding-bottom: 1rem;
+  }
+
+  .comments-list .nested-comments {
+    border-left: 2px solid #dee2e6;
+    padding-left: 1rem;
+  }
+</style>
---
+++ server/api/article/getArticle.get.ts
@@ -0,0 +1,23 @@
+import { articles } from "../articles";
+export default defineEventHandler((event) => {
+  const query = getQuery(event);
+  const articleId = Number(query.id);
+
+  if (!articleId) {
+    throw createError({
+      statusCode: 400,
+      statusMessage: "○缺少文章ID",
+    });
+  }
+
+  const article = articles.value.find((article) => article.id === articleId);
+
+  if (!article) {
+    throw createError({
+      statusCode: 404,
+      statusMessage: "找不到文章",
+    });
+  }
+
+  return article;
+});
